4e3a5efb883348f504ae71e7bbaa39f90b7c4d4f
pumar <rodrigopumar@gmail.com>
2019-12-05 17:18:36 -0300

    removes code on javascript that didnt worked

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 App = {
   web3Provider: null,
   contracts: {},
-
+  account: "0x0",
   init: async function() {
     return await App.initWeb3();
   },
@@ -31,93 +31,37 @@ App = {
   },
 
   render: function() {
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
     //Projeto Inicia aqui
     App.contracts.Chainclub.deployed()
       .then(function(instance) {
-        _instance = instance;
         return instance;
       })
       .then(function(chainclubInstance) {
         var count = chainclubInstance.getPollCount();
-        makeUL(count, chainclubInstance);
+        console.log(count);
       });
   }
 };
 
-var _instance = null;
-
 window.setMessage = function() {
-  console.log(_instance);
   let message = $("#new_message").val();
-  _instance.startPoll(2, 2, message, 5, 0);
-
-  _instance
-    .send({
-      from: window.coinbase
+  App.contracts.Chainclub.deployed()
+    .then(function(instance) {
+      return instance.startPoll(1, 1, message, 1, 1, { from: App.account });
     })
-    .then(function(res) {
-      console.log(res);
+    .then(function(result) {
+      console.log("done");
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 };
-function makeUL(count, chainclubInstance) {
-  console.log("makeUL");
-  // Create the list element:
-  var list = document.createElement("ul");
-  var fetchPosts = new Promise(function(resolve, rejected) {
-    var posts = [];
-    for (var i = 0; i < count; i++) {
-      var contentsPromise = chainclubInstance.getPollSubject(i).call();
-      var votesPromise = chainclubInstance.getPollVotes(i).call();
-      var post = Promise.all([votesPromise, contentsPromise, i]).then(function([
-        votes,
-        content,
-        index
-      ]) {
-        return {
-          votes,
-          content,
-          index
-        };
-      });
-      posts.push(post);
-    }
-    Promise.all(posts).then(posts => resolve(posts));
-  });
-  fetchPosts
-    .then(posts => {
-      return posts.sort(compare);
-    })
-    .then(function(posts) {
-      for (var i = 0; i < count; i++) {
-        var item = document.createElement("li");
-        item.appendChild(document.createTextNode(posts[i].content));
-        (button = document.createElement("input")),
-          (br = document.createElement("br"));
-        button.type = "button";
-        button.value = posts[i].votes;
-        button.myParam = posts[i].index;
-        button.addEventListener("click", vote, false);
-        item.appendChild(button);
-        list.appendChild(item);
-      }
-    });
-  return list;
-}
-
-function vote(index) {
-  window.Chainclub.methods
-    .payMessager(index.target.myParam, "test", 1)
-    .send({
-      from: window.coinbase
-    })
-    .then(function(res) {
-      console.log(index.target.myParam);
-      alert("You voted on this poll");
-    });
-}
-function compare(a, b) {
-  return b.votes - a.votes;
-}
 $(function() {
   $(window).load(function() {
     App.init();
