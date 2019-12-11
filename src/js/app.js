06b82c86fd38d9fcff253d94fcae663e577a7aa6
pumar <rodrigopumar@gmail.com>
2019-12-11 18:34:06 -0300

    voto bool interface

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ App = {
   },
 
   initWeb3: function() {
-    console.log(web3)
+    console.log(web3);
     // TODO: refactor conditional
     if (typeof web3 !== "undefined") {
       // If a web3 instance is already provided by Meta Mask.
@@ -30,7 +30,7 @@ App = {
       App.contracts.Chainclub = TruffleContract(Chainclub);
       App.contracts.Chainclub.setProvider(App.web3Provider);
 
-      console.log(App.contracts)
+      console.log(App.contracts);
       return App.render();
     });
   },
@@ -46,128 +46,135 @@ App = {
     //Projeto Inicia aqui
     App.contracts.Chainclub.deployed()
       .then(function(instance) {
-        console.log(instance)
+        console.log(instance);
         return instance;
       })
-      .then(async function(chainclubInstance) {        
+      .then(async function(chainclubInstance) {
+        var count = 0;
+        var end = false;
+        while (1) {
+          const subject = await chainclubInstance
+            .getBooleanPollSubject(count)
+            .then(res => {
+              return res;
+            })
+            .catch(err => {
+              end = true;
+            });
 
-        var count = 0
-        var end = false
-        while(1) {
-          const subject = await chainclubInstance.getBooleanPollSubject(count)
-          .then(res => {
-            return res
-          })
-          .catch(err => {
-            end = true
-          });
-
-          if(end) {
-            break
+          if (end) {
+            break;
           }
 
-          console.log(subject)
+          console.log(subject);
 
-          const num_votes = await chainclubInstance.getBooleanPollVotesCount(count)
-          .then(res => {
-            return res.c[0]
-          });
+          const num_votes = await chainclubInstance
+            .getBooleanPollVotesCount(count)
+            .then(res => {
+              return res.c[0];
+            });
 
-          console.log(num_votes)
+          console.log(num_votes);
 
           count++;
 
           poll = {
             subject: subject,
             num_votes: num_votes,
-            type: 'boolean'
-          }
+            type: "boolean"
+          };
 
-          App.polls.push(poll)
+          App.polls.push(poll);
         }
 
-        count = 0
-        end = false
-        while(1) {
-          const subject = await chainclubInstance.getQuantityPollSubject(count)
-          .then(res => {
-            return res
-          })
-          .catch(err => {
-            end = true
-          });
+        count = 0;
+        end = false;
+        while (1) {
+          const subject = await chainclubInstance
+            .getQuantityPollSubject(count)
+            .then(res => {
+              return res;
+            })
+            .catch(err => {
+              end = true;
+            });
 
-          if(end) {
-            break
+          if (end) {
+            break;
           }
 
-          const num_votes = await chainclubInstance.getQuantityPollVotesCount(count)
-          .then(res => {
-            return res.c[0]
-          });
+          const num_votes = await chainclubInstance
+            .getQuantityPollVotesCount(count)
+            .then(res => {
+              return res.c[0];
+            });
 
-          const bottom_limit = await chainclubInstance.getQuantityPollBottomLimit(count)
-          .then(res => {
-            return res.c[0]
-          });
+          const bottom_limit = await chainclubInstance
+            .getQuantityPollBottomLimit(count)
+            .then(res => {
+              return res.c[0];
+            });
 
-          const top_limit = await chainclubInstance.getQuantityPollTopLimit(count)
-          .then(res => {
-            return res.c[0]
-          });
+          const top_limit = await chainclubInstance
+            .getQuantityPollTopLimit(count)
+            .then(res => {
+              return res.c[0];
+            });
 
           count++;
 
           poll = {
             subject: subject,
             num_votes: num_votes,
-            type: 'quantity',
+            type: "quantity",
             bottom_limit: bottom_limit,
             top_limit: top_limit
-          }
+          };
 
-          App.polls.push(poll)
+          App.polls.push(poll);
         }
 
-        count = 0
-        end = false
-        while(1) {
-          const subject = await chainclubInstance.getOptionsPollSubject(count)
-          .then(res => {
-            return res
-          })
-          .catch(err => {
-            end = true
-          });
+        count = 0;
+        end = false;
+        while (1) {
+          const subject = await chainclubInstance
+            .getOptionsPollSubject(count)
+            .then(res => {
+              return res;
+            })
+            .catch(err => {
+              end = true;
+            });
 
-          if(end) {
-            break
+          if (end) {
+            break;
           }
 
-          const num_votes = await chainclubInstance.getOptionsPollVotesCount(count)
-          .then(res => {
-            return res.c[0]
-          });
+          const num_votes = await chainclubInstance
+            .getOptionsPollVotesCount(count)
+            .then(res => {
+              return res.c[0];
+            });
 
           count++;
 
           poll = {
             subject: subject,
             num_votes: num_votes,
-            type: 'options'
-          }
+            type: "options"
+          };
 
-          App.polls.push(poll)
+          App.polls.push(poll);
         }
 
-        console.log(App.polls)
+        console.log(App.polls);
 
-        for(var i = 0; i < App.polls.length; i++) {
-          if(App.polls[i].type == 'boolean') {
-            $("#polls-list").append(`<li>${App.polls[i].subject} (${App.polls[i].num_votes} votes) <button>Yes</button> <button>No</button></li>`)
-          }
-
-          else if(App.polls[i].type == 'quantity') {
+        for (var i = 0; i < App.polls.length; i++) {
+          if (App.polls[i].type == "boolean") {
+            $("#polls-list").append(
+              `<li>${App.polls[i].subject} (${App.polls[i].num_votes} votes) <button index=${i} vote=true class=yes >Yes</button> <button index=${i} class=no vote=false >No</button></li>`
+            );
+          } else if (App.polls[i].type == "quantity") {
             $("#polls-list").append(`
             <li>${App.polls[i].subject} (${App.polls[i].num_votes} votes)
               <form action="#">
@@ -177,19 +184,39 @@ App = {
                   <button>Confirm</button>
                 </p>
               </form>
-            </li>`)
+            </li>`);
           }
         }
       });
+
+    $("body").on("click", ".yes", function(e) {
+      var value = $(this).attr("index");
+      vote(value, true);
+    });
+    $("body").on("click", ".no", function(e) {
+      var value = $(this).attr("index");
+      vote(value, false);
+    });
+    function vote(value, bool) {
+      App.contracts.Chainclub.deployed().then(function(instance) {
+        instance
+          .voteOnBooleanPool(value, bool, { from: App.account })
+          .then(function(result) {
+            console.log(result);
+          });
+      });
+    }
   },
 
   booleanpoll: function() {
     var booleanpoll = $("#boolen-poll").val();
 
-    App.contracts.Chainclub.deployed().then(function(instance){
-      instance.startBooleanPoll(booleanpoll, {from: App.account}).then(function(result){
-        console.log(result);
-      });
+    App.contracts.Chainclub.deployed().then(function(instance) {
+      instance
+        .startBooleanPoll(booleanpoll, { from: App.account })
+        .then(function(result) {
+          console.log(result);
+        });
     });
   },
 
@@ -198,27 +225,16 @@ App = {
     var superiorlimit = $("#superior-limit").val();
     var inferiorlimtit = $("#inferior-limit").val();
 
-    App.contracts.Chainclub.deployed().then(function(instance){
-      instance.startQuantityPoll(booleanpoll,inferiorlimtit,superiorlimit, {from: App.account}).then(function(result){
-        console.log(result);
-      });
+    App.contracts.Chainclub.deployed().then(function(instance) {
+      instance
+        .startQuantityPoll(booleanpoll, inferiorlimtit, superiorlimit, {
+          from: App.account
+        })
+        .then(function(result) {
+          console.log(result);
+        });
     });
   }
-
-};
-
-window.setMessage = function() {
-  let message = $("#new_message").val();
-  App.contracts.Chainclub.deployed()
-    .then(function(instance) {
-      return instance.startQuantityPoll( message );
-    })
-    .then(function(result) {
-      console.log("done");
-    })
-    .catch(function(err) {
-      console.log(err);
-    });
 };
 
 $(function() {
